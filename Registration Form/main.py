60d950cd770f0f4c66c0206b26eab1799c6b147a
DRASH7775 <panchaldrashti7775@gmail.com>
2019-05-31 15:10:44 +0530

    initalized update

---
+++ Registration Form/main.py
@@ -0,0 +1,10 @@
+# from flask import Flask, render_template
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home_page():
+#     return render_template("main.html")
+
+# if __name__ == '__main__':
+#     app.run(host='192.168.1.38', port=8080, debug=True)
+
